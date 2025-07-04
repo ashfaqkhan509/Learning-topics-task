daaca2e2115f0fe22b11e5512802bdb7d97516ed
ashi <ashfaq319535@gmail.com>
2025-07-04 19:10:17 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Learning-topics-task
---
+++ functool/lru-cache.py
@@ -0,0 +1,29 @@
+import time
+from functools import lru_cache
+
+
+def fibonancy(n):
+    if n < 2:
+        return n
+    return fibonancy(n-1) + fibonancy(n-2)
+
+
+start = time.time()
+fibonancy(20)
+end = time.time()
+
+
+print(f"{fibonancy.__name__} function take {(end-start) * 1000}ms during execution without cache.")
+
+
+@lru_cache(maxsize=None)
+def fibonancy(n):
+    if n < 2:
+        return n
+    return fibonancy(n-1) + fibonancy(n-2)
+
+start = time.time()
+fibonancy(20)
+end = time.time()
+
+print(f"{fibonancy.__name__} function take {(end-start) * 1000}ms during execution with cache.")
