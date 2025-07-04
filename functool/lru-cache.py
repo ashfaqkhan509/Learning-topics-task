03ade44fb47ddc8344658fc993ba6d0d785aee4e
ashi <ashfaq319535@gmail.com>
2025-07-04 20:33:59 +0500

    add new task of functools module using partial library and code checker

--- functool/lru-cache.py
+++ functool/lru-cache.py
@@ -11,7 +11,6 @@ start = time.time()
 fibonancy(20)
 end = time.time()
 
-
 print(f"{fibonancy.__name__} function take {(end-start) * 1000}ms during execution without cache.")
 
 
--- functool/parital_task.py
+++ functool/parital_task.py
@@ -1,5 +1,6 @@
 from functools import partial
 
+
 def power(a, b):
     return a**b
 
@@ -7,4 +8,7 @@ pow_of_2 = partial(power, b=2)
 pow_of_5 = partial(power, b=5)
 pow_of_10 = partial(power, b=10)
 
-print(pow_of_2(5))
+print(f"5 power 2 = {pow_of_2(5)}")
+print(f"4 power 5 = {pow_of_5(4)}")
+print(f"3 power 10 = {pow_of_10(3)}")
+
